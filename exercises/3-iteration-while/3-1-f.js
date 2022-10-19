33b51d31abd425599eda4ea7b08a1981965ecce3
Karsten Verherstraeten <karsten.verherstraeten@student.ehb.be>
2022-10-19 16:14:17 +0200

    F Done

---
+++ exercises/3-iteration-while/3-1-f.js
@@ -0,0 +1,21 @@
+"use strict";
+import context from "../../scripts/context.js";
+
+draw();
+
+function draw() {
+    context.lineWidth = 2;
+    context.strokeStyle = "white";
+    context.fillStyle = "#3498DB";
+    context.fillRect(50, 50, 300, 300);
+    drawDiagonalSquares();
+}
+
+function drawDiagonalSquares() {
+    let i = 0;
+    let space = 25
+    while (i <= 7) {
+        context.strokeRect( 75+ (i * space), 75 + (i * space), 75, 75);
+        i++;
+    }
+}
