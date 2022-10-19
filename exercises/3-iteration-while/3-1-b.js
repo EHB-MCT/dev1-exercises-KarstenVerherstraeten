0b2ca69b0fc566b4dd73be1066eb820837d2dc03
Karsten Verherstraeten <karsten.verherstraeten@student.ehb.be>
2022-10-19 14:40:43 +0200

    B done

---
+++ exercises/3-iteration-while/3-1-b.js
@@ -0,0 +1,25 @@
+"use strict";
+import context from "../../scripts/context.js";
+import * as Utils from "../../scripts/utils.js";
+
+draw();
+
+function draw() {
+    context.lineWidth = 2;
+    context.strokeStyle = "white";
+    context.fillStyle = "#3498DB";
+    context.fillRect(50, 50, 300, 300);
+    drawGrid();
+}
+
+function drawGrid() {
+    let i = 0;
+    let space = 50
+    while (i <=6) {
+        let x = 25 + (i * space);
+        let y = x;
+        Utils.drawLine(75, y, 325, y);
+        Utils.drawLine(x, 75, x, 325);
+        i++  ;
+    }
+}
